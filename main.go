90eb7ed989eb24078e0fdecb1aebbe7acedc8747
Nathan Doolan <nathan.doolan@gentrack.com>
2023-11-08 22:43:28 +1100

    Add crawler to crawl commits recursively

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -16,13 +17,47 @@ func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("This program expects at least 1 string argument.")
 	}
-	
-	content, err := scrape(os.Args[1])
-	if err != nil {
-		fmt.Println("Error scraping:", err)
+	startURL := os.Args[1]
+
+	maxDepth := 1
+	if len(os.Args) >= 3 {
+		var err error
+		maxDepth, err = strconv.Atoi(os.Args[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	visitedUrls := make(map[string]bool)
+	crawl(startURL, maxDepth, visitedUrls)
+}
+
+func crawl(url string, depth int, visitedUrls map[string]bool) {
+	if depth <= 0 {
 		return
 	}
-	fmt.Println(extractLinks(content))
+	fmt.Println("Crawling", url)
+
+	content, err := scrape(url)
+	if err != nil {
+		fmt.Println("Error scraping", url, ":", err)
+		return
+	}
+
+	visitedUrls[url] = true
+
+	links, err := extractLinks(content)
+	if err != nil {
+		fmt.Println("Error extracting links from", url, ":", err)
+	}
+
+	for _, link := range links {
+		if !visitedUrls[link] {
+			fmt.Println("Found", link)
+			visitedUrls[link] = false
+			crawl(link, depth-1, visitedUrls)
+		}
+	}
 }
 
 func scrape(url string) ([]byte, error) {
@@ -30,8 +65,12 @@ func scrape(url string) ([]byte, error) {
 	if getErr != nil {
 		return nil, getErr
 	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("HTTP response error: %s", res.Status)
+	}
 	content, readErr := io.ReadAll(res.Body)
-	res.Body.Close()
 	if readErr != nil {
 		return nil, readErr
 	}
--- main_test.go
+++ main_test.go
@@ -21,3 +21,45 @@ func TestScrapeAndExtractLinks(t *testing.T) {
 			links, parseErr, want)
 	}
 }
+
+// TestShallowCrawl calls main.crawl with a url and a depth of 1, checking
+// for a valid mutation of VisitedUrls.
+func TestShallowCrawl(t *testing.T) {
+	visitedUrls := make(map[string]bool)
+	crawl("http://quotes.toscrape.com/author/Albert-Einstein/", 1, visitedUrls)
+
+	want := make(map[string]bool)
+	want["http://quotes.toscrape.com/author/Albert-Einstein/"] = true
+	want["https://www.goodreads.com/quotes"] = false
+	want["https://www.zyte.com"] = false
+
+	fail := false
+	if len(visitedUrls) != len(want) {
+		fail = true
+	}
+	for key, visitValue := range visitedUrls {
+		wantValue, ok := want[key]
+		if !ok || visitValue != wantValue {
+			fail = true
+			break
+		}
+	}
+	if fail {
+		t.Fatalf(`crawl("http://quotes.toscrape.com/author/Albert-Einstein/", 1, visitedUrls),
+        did not result in visitedUrls = %#v, instead it was %#v`, want, visitedUrls)
+	}
+}
+
+// TestCrawl calls main.crawl with a url and a depth of 3, checking
+// for a valid mutation of VisitedUrls.
+func TestCrawl(t *testing.T) {
+	visitedUrls := make(map[string]bool)
+	crawl("http://www.example.com", 3, visitedUrls)
+
+	wantedLen := 105
+
+	if len(visitedUrls) != wantedLen {
+		t.Fatalf(`crawl("http://www.example.com", 3, visitedUrls), did not result in
+        len(visitedUrls) = %#v, instead it was %#v`, wantedLen, len(visitedUrls))
+	}
+}
